ae4ab4aad68b5590a7236a4ba5b1b1db405b547e
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-14 04:59:17 -0300

    🔀 chore: update route names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Jobs from "./components/jobs/Jobs";
-import Login from "./components/login/Login";
-import Register from "./components/register/Register";
-import CreateJob from "./components/createJob/CreateJob";
+import Home from "./pages/home/Home";
+import Jobs from "./pages/jobs/Jobs";
+import Recruiter from "./pages/recruiter/Recruiter";
+import User from "./pages/user/User";
+import CreateJob from "./pages/createjob/CreateJob";
 
 function App() {
   const isRecruiter = true; // Altere para false para simular um usuário comum
@@ -14,8 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/jobs" element={<Jobs isRecruiter={isRecruiter} />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/recruiter" element={<Recruiter />} />
+          <Route path="/user" element={<User />} />
           <Route path="/create-job" element={<CreateJob />} />
         </Routes>
       </div>
