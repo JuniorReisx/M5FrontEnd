8abe0bfdda3dbb3af252978e8e95193b4d9bc9ae
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-31 22:15:27 -0300

    icon

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon.png',
+            src: '/public/icon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'icon.png',
+            src: '/public/icon.png',
             sizes: '512x512',
             type: 'image/png',
           },
