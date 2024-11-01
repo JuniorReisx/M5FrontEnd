db4a8ce85b150dd189b3231da46fd40b5244b4be
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-31 22:01:07 -0300

    add icon.png

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'iconct.png',
+            src: 'icon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'iconct.png',
+            src: 'icon.png',
             sizes: '512x512',
             type: 'image/png',
           },
