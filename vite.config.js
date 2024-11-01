a54fdc10e528cbc9d1238ea185a5489885cf07c7
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-31 21:28:31 -0300

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TechNBlack',
+        short_name: 'TechNBlack',
+        description: 'Descrição do PWA',
+        theme_color: '#000',
+        background_color: '#000',
+        display: 'fullscreen',
+        start_url: '/',
+        icons: [
+          {
+            src: 'iconct.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'iconct.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
